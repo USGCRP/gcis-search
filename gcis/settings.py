73532208f5d86cfeb39c31e3f33da2d5993e0b50
Gerald Manipon <pymonger@gmail.com>
2014-09-18 19:51:59 +0000

    added import of merged CEOS-GCMD metadata into ES

--- gcis/settings.py
+++ gcis/settings.py
@@ -3,9 +3,16 @@ class Config(object):
     #GCIS_REST_URL = 'http://127.0.0.1:3000'
     GCIS_REST_URL = 'http://data.globalchange.gov'
     ELASTICSEARCH_URL = 'http://127.0.0.1:9200'
+
+    # for GCIS app
     GCIS_ELASTICSEARCH_INDEX = 'gcis'
     GCIS_ELASTICSEARCH_SETTINGS = '../config/es_settings-gcis.json'
     GCIS_ELASTICSEARCH_MAPPING = '../config/es_mapping-gcis.json'
+
+    # CEOS-GCMD merged instruments/platforms app
+    CEOS_GCMD_ELASTICSEARCH_INDEX = 'ceos_gcmd'
+    CEOS_GCMD_ELASTICSEARCH_SETTINGS = '../config/es_settings-ceos_gcmd.json'
+    CEOS_GCMD_ELASTICSEARCH_MAPPING = '../config/es_mapping-ceos_gcmd.json'
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
